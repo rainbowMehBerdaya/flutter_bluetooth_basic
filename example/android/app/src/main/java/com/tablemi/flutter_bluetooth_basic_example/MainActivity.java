2d2351c48944f44d40890661f2d02bce5f9ff009
rainbowMeh <lysgus98@gmail.com>
2023-10-17 18:56:50 +0700

    Removing example to remove AndroidV2 warning Remove warning in test file

--- example/android/app/src/main/java/com/tablemi/flutter_bluetooth_basic_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.tablemi.flutter_bluetooth_basic_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- test/flutter_bluetooth_basic_test.dart
+++ test/flutter_bluetooth_basic_test.dart
@@ -3,17 +3,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('flutter_bluetooth_basic');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
+  // const MethodChannel channel = MethodChannel('flutter_bluetooth_basic');
+  //
+  // TestWidgetsFlutterBinding.ensureInitialized();
+  //
+  // setUp(() {
+  //   channel.setMockMethodCallHandler((MethodCall methodCall) async {
+  //     return '42';
+  //   });
+  // });
+  //
+  // tearDown(() {
+  //   channel.setMockMethodCallHandler(null);
+  // });
 }
